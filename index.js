e4b82e5091b6f24a3f83fed2fd6c329d795502a1
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-06-18 16:10:18 -0500

    added questions for user to answer

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const inquirer = require('inquirer') 
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter project title",
+    },
+
+    {
+        type: "input",
+        name: "Description",
+        message: "Enter project description",
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter steps for installation",
+    },
+
+    {
+        type: "input",
+        name: "Usage",
+        message: "Describe what project will be used for",
+    },
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Enter contribution info",
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "For questions (email)",
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "For questions (github)",
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "Type of license used",
+        choices: ['None', 'ISC', 'MIT'],
+        filter(val) {
+            return val;
+        }
+    }
+]
